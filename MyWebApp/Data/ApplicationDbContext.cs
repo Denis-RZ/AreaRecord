e139d8950ae0adbd492015ea2f125c301fc9f141
Denis-RZ <77514212+Denis-RZ@users.noreply.github.com>
2025-06-10 22:44:41 +0800

    Remove binary favicon and update gitignore

---
+++ MyWebApp/Data/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+using Microsoft.EntityFrameworkCore;
+
+namespace MyWebApp.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        // Add DbSet<> properties here
+    }
+}
