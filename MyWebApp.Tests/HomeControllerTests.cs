049a0bcfd7655474772427e01067c954f750eb71
Denis-RZ <77514212+Denis-RZ@users.noreply.github.com>
2025-06-11 12:53:46 +0800

    Redesign site as extension landing page

--- MyWebApp.Tests/HomeControllerTests.cs
+++ MyWebApp.Tests/HomeControllerTests.cs
@@ -1,6 +1,8 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging.Abstractions;
 using MyWebApp.Controllers;
+using Microsoft.EntityFrameworkCore;
+using MyWebApp.Data;
 using Xunit;
 
 public class HomeControllerTests
@@ -8,7 +10,11 @@ public class HomeControllerTests
     [Fact]
     public void Index_ReturnsView()
     {
-        var controller = new HomeController(NullLogger<HomeController>.Instance);
+        var options = new DbContextOptionsBuilder<MyWebApp.Data.ApplicationDbContext>()
+            .UseInMemoryDatabase("IndexDb")
+            .Options;
+        using var context = new MyWebApp.Data.ApplicationDbContext(options);
+        var controller = new HomeController(NullLogger<HomeController>.Instance, context);
         var result = controller.Index();
         Assert.IsType<ViewResult>(result);
     }
@@ -16,8 +22,24 @@ public class HomeControllerTests
     [Fact]
     public void Privacy_ReturnsView()
     {
-        var controller = new HomeController(NullLogger<HomeController>.Instance);
+        var options = new DbContextOptionsBuilder<MyWebApp.Data.ApplicationDbContext>()
+            .UseInMemoryDatabase("PrivacyDb")
+            .Options;
+        using var context = new MyWebApp.Data.ApplicationDbContext(options);
+        var controller = new HomeController(NullLogger<HomeController>.Instance, context);
         var result = controller.Privacy();
+        Assert.IsType<ViewResult>(result);
+    }
+
+    [Fact]
+    public void Faq_ReturnsView()
+    {
+        var options = new DbContextOptionsBuilder<MyWebApp.Data.ApplicationDbContext>()
+            .UseInMemoryDatabase("FaqDb")
+            .Options;
+        using var context = new MyWebApp.Data.ApplicationDbContext(options);
+        var controller = new HomeController(NullLogger<HomeController>.Instance, context);
+        var result = controller.Faq();
         Assert.IsType<ViewResult>(result);
     }
 }
